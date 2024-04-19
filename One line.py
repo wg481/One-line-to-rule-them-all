c139e1edabac6ad5acddedeb6eb6b1c48015d8f2
WG481 <77253779+wg481@users.noreply.github.com>
2024-04-19 14:43:42 -0700

    Add files via upload

---
+++ One line.py
@@ -0,0 +1,1 @@
+print(str(round(float(float(input("Initial value: ")) * (1 + (float(input("Decimal interest rate: "))) / float(input("Comps a year: "))) ** (float(input("Again: ")) * float(input("Years: ")))), 2)))
